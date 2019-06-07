d4d465c1dfefd69cbd5e770847001a4f11e6dd7d
lutomskaya <lutomskaya@gmail.com>
2019-06-07 18:37:33 +0300

    Поправки

--- js/stat.js
+++ js/stat.js
@@ -21,8 +21,19 @@ function renderText(ctx, x, y, text) {
   ctx.fillText(text, x, y);
 }
 
-function renderSingleBar(ctx, x, y, columnHeight) {
-  ctx.fillRect(x, y, COLUMN_WIDTH, columnHeight);
+function renderSingleBar(ctx, index, name, time, max) {
+  var coordinateX = CLOUD_X + COLUMN_BETWEEN * index + COLUMN_WIDTH * (index + 1);
+  var columnHeight = COLUMN_HEIGHT * (time / max);
+  var coordinateY = (CLOUD_HEIGHT - FONT_GAP) - columnHeight;
+
+  ctx.fillStyle = '#000000';
+
+  ctx.fillText(name, coordinateX, CLOUD_HEIGHT);
+  ctx.fillText(Math.floor(time), coordinateX, coordinateY - GAP);
+
+  ctx.fillStyle = (name === 'Вы') ? 'rgba(255, 0, 0, 1)' : 'rgba(0, 0, 255, ' + (Math.random() + 0.1).toFixed(1) + ')';
+
+  ctx.fillRect(coordinateX, coordinateY, COLUMN_WIDTH, columnHeight);
 }
 
 function getMaxElement(arr) {
@@ -38,26 +49,15 @@ function getMaxElement(arr) {
 }
 
 window.renderStatistics = function (ctx, names, times) {
+  var maxTime = getMaxElement(times);
+
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
 
   renderText(ctx, CLOUD_X + GAP + FONT_GAP, CLOUD_Y + GAP + FONT_GAP, 'Ура вы победили!');
   renderText(ctx, CLOUD_X + GAP + FONT_GAP, CLOUD_Y + GAP + FONT_GAP * 2, 'Список результатов:');
 
-  var maxTime = getMaxElement(times);
-
   for (var j = 0; j < names.length; j++) {
-
-    var coordinateX = CLOUD_X + COLUMN_BETWEEN * j + COLUMN_WIDTH * (j + 1);
-    var coordinateY = (CLOUD_HEIGHT - FONT_GAP) - COLUMN_HEIGHT * (times[j] / maxTime);
-    var columnHeight = COLUMN_HEIGHT * (times[j] / maxTime);
-    ctx.fillStyle = '#000000';
-
-    ctx.fillText(names[j], coordinateX, CLOUD_HEIGHT);
-    ctx.fillText(Math.floor(times[j]), coordinateX, coordinateY - GAP);
-
-    ctx.fillStyle = (names[j] === 'Вы') ? 'rgba(255, 0, 0, 1)' : 'rgba(0, 0, 255, ' + (Math.random() + 0.1).toFixed(1) + ')';
-
-    renderSingleBar(ctx, coordinateX, coordinateY, columnHeight);
+    renderSingleBar(ctx, j, names[j], times[j], maxTime);
   }
 };
